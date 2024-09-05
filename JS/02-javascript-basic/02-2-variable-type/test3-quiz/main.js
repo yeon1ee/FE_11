46d7be879ad6f5189af5b8720843f16d4026975d
yeon1ee <carpediem.y13@gmail.com>
2024-09-05 11:05:12 +0900

    quiz 수정

--- JS/02-javascript-basic/02-2-variable-type/test3-quiz/main.js
+++ JS/02-javascript-basic/02-2-variable-type/test3-quiz/main.js
@@ -9,7 +9,9 @@ const introduce = () => {
     let wish = '6개월동안 열심히 해서 성장하고 싶습니다.';
     let message = '다함께 열심히 해봅시다! 화이팅입니다!';
 
-    document.write(NAME + "<br>" + self + "<br>" + wish + "<br>" + message);
+    document.write("이름: " + NAME + "<br>" + "자기소개: " + self + "<br>" +
+        "프론트엔드 스쿨 11기를 통해 이루고 싶은 점: " + wish + "<br>" + 
+        "동료들과 나에게 보내는 응원메세지: " + message);
 }
 
 introduce();
