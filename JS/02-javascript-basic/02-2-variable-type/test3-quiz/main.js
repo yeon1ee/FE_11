88342ab91adc4d08c6d66ce01585813c33861450
yeon1ee <carpediem.y13@gmail.com>
2024-09-05 10:43:49 +0900

    자기소개 document.write 으로 작성해보기

---
+++ JS/02-javascript-basic/02-2-variable-type/test3-quiz/main.js
@@ -0,0 +1,15 @@
+const NAME = '이희연';
+let self = '안녕하세요 프론트엔드 개발자를 목표로 하는 이희연입니다.';
+let wish = '6개월동안 열심히 해서 성장하고 싶습니다.';
+let message = '다함께 열심히 해봅시다! 화이팅입니다!';
+
+const introduce = () => {
+    const NAME = '이희연';
+    let self = '안녕하세요 프론트엔드 개발자를 목표로 하는 이희연입니다.';
+    let wish = '6개월동안 열심히 해서 성장하고 싶습니다.';
+    let message = '다함께 열심히 해봅시다! 화이팅입니다!';
+
+    document.write(NAME + "<br>" + self + "<br>" + wish + "<br>" + message);
+}
+
+introduce();
