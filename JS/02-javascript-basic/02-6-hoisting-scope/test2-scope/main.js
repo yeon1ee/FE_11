a2fc56dad9c862f098200e292eba770cc9fc4e74
yeon1ee <carpediem.y13@gmail.com>
2024-09-12 09:44:13 +0900

    scope 함수 실습

--- JS/02-javascript-basic/02-6-hoisting-scope/test2-scope/main.js
+++ JS/02-javascript-basic/02-6-hoisting-scope/test2-scope/main.js
@@ -25,3 +25,89 @@ console.log(`out function ${data4}`);   //out function 홍길동
 //scope가 다르다면 변수 중복 선언 가능하다.
 //local 변수 우선
 //변수명을 의미있는 단어로 주는 것이 기본임으로 변수명을 중복해서 local 변수로 자주 쓴다.
+
+//함수 중복 선언
+function myFun1() { console.log('step1'); }
+function myFun1() { console.log('step2'); }
+
+myFun1();   //step2
+
+var myFun2 = function() { console.log('step1'); }
+var myFun2 = function() { console.log('step2'); }
+
+myFun2();
+
+let myFun3 = function () { console.log('step1'); }
+//let myFun3 = function () { console.log('step2'); }
+
+myFun3();   //error
+
+//함수 중복 선언: 선언식 함수와 var에 대입되는 표현식만 중복 선언 가능하다.
+
+//모든 software language에서는 하나의 scope 내에 선언된 변수, 함수는 그 스코프에서만 사용 가능하다
+
+// var name1 = '홍길동';
+
+// const someFun = () => {
+//     var name1 = '김길동';
+//     console.log(`in someFun 1, name1 = ${name1}`);
+
+//     for(let i = 0; i < 1; i++) {
+//         var name1 = '이길동'
+//         console.log(`in someFun, in for, name1 = ${name1}`);
+//     }
+//     console.log(`in someFun 2, name1 = ${name1}`);
+
+//     if(true) {
+//         var name1 = '박길동';
+//         console.log(`in someFun in if, name1 = ${name1}`);
+//     }
+//     console.log(`in someFun 3, name1 = ${name1}`);
+// }
+
+// someFun();
+// console.log(`out someFun, name1 = ${name1}`);
+
+/*
+김길동
+이길동
+이길동
+박길동
+박길동
+홍길동
+*/
+
+//var로 선언된 변수, 함수 scope만 지원한다.
+//for, if는 지원하지 않는다.
+
+let name1 = '홍길동';
+
+const someFun = () => {
+    let name1 = '김길동';
+    console.log(`in someFun 1, name1 = ${name1}`);
+
+    for(let i = 0; i < 1; i++) {
+        let name1 = '이길동'
+        console.log(`in someFun, in for, name1 = ${name1}`);
+    }
+    console.log(`in someFun 2, name1 = ${name1}`);
+
+    if(true) {
+        let name1 = '박길동';
+        console.log(`in someFun in if, name1 = ${name1}`);
+    }
+    console.log(`in someFun 3, name1 = ${name1}`);
+}
+
+someFun();
+console.log(`out someFun, name1 = ${name1}`);
+
+/*
+김길동
+이길동
+김길동
+박길동
+김길동
+홍길동 */
+
+//let은 함수, for, if 스코프를 지원한다.
