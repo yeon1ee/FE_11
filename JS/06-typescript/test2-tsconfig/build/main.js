82a6832b33adfadc791d25c4cf3c4c7ec83bfb49
yeon1ee <carpediem.y13@gmail.com>
2024-10-10 16:18:40 +0900

    typescript 실습

---
+++ JS/06-typescript/test2-tsconfig/build/main.js
@@ -0,0 +1,6 @@
+var userName = '홍길동';
+function sayHello() {
+    console.log("Hello ".concat(userName));
+}
+sayHello();
+//# sourceMappingURL=main.js.map
---
+++ JS/06-typescript/test2-tsconfig/src/main.ts
@@ -0,0 +1,7 @@
+let userName: string = '홍길동';
+
+function sayHello() {
+    console.log(`Hello ${userName}`);
+}
+
+sayHello();
